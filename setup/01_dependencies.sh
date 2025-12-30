13270036c4906dbee45549c4a670071319cdf26e
malay <malay.hazarika@gmail.com>
2025-12-30 11:38:08 +0530

    waybar config

--- setup/01_dependencies.sh
+++ setup/01_dependencies.sh
@@ -3,7 +3,6 @@
 
 general_tools=(
     stow
-    yay
     unzip
     wget
     curl   
@@ -22,6 +21,29 @@ core_dependencies=(
 )
 
 
-sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm "${general_tools[@]}"
-sudo pacman -S --noconfirm "${core_dependencies[@]}"
+_isInstalled() {
+    package="$1"
+    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")"
+    if [ -n "${check}" ]; then
+        echo 0
+        return #true
+    fi
+    echo 1
+    return #false
+}
+
+_installPackages() {
+    for pkg; do
+        if [[ $(_isInstalled "${pkg}") == 0 ]]; then
+            echo ":: ${pkg} is already installed."
+            continue
+        fi
+        yay --noconfirm -S "${pkg}"
+    done
+}
+
+
+
+sudo yay -Syu --noconfirm
+_installPackages "${general_tools[@]}"
+_installPackages "${core_dependencies[@]}"
