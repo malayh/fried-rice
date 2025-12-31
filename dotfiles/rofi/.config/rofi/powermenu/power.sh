49aaf3d1d758696b8dba139fc93c77282147a0ae
malay <malay.hazarika@gmail.com>
2025-12-31 18:41:41 +0530

    Universal menu

--- dotfiles/rofi/.config/rofi/powermenu/power.sh
+++ dotfiles/rofi/.config/rofi/powermenu/power.sh
@@ -1,54 +1,24 @@
 #!/usr/bin/env bash
 
-dir="$HOME/.config/rofi/powermenu"
+source ~/.config/rofi/common/generic.sh
 
 # CMDs
 uptime="`uptime -p | sed -e 's/up //g'`"
-host=`hostname`
 
 # Options
 shutdown=' Power off'
 suspend=' Suspend'
 reboot=' Reboot'
 lock=' Lock'
-yes=''
-no='󰜺'
 
-# Rofi CMD
-rofi_cmd() {
-	rofi -dmenu \
-		-p "Uptime: $uptime" \
-		-mesg "Uptime: $uptime" \
-		-theme ${dir}/theme.rasi
-}
-
-# Confirmation CMD
-confirm_cmd() {
-	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
-		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
-		-theme-str 'listview {columns: 2; lines: 1;}' \
-		-theme-str 'element-text {horizontal-align: 0.5;}' \
-		-theme-str 'textbox {horizontal-align: 0.5;}' \
-		-dmenu \
-		-p 'Confirmation' \
-		-mesg 'Are you Sure?' \
-		-theme ${dir}/theme.rasi
-}
-
-# Ask for confirmation
-confirm_exit() {
-	echo -e "$yes\n$no" | confirm_cmd
-}
-
-# Pass variables to rofi dmenu
-run_rofi() {
-	echo -e "$lock\n$suspend\n$reboot\n$shutdown" | rofi_cmd
-}
+yes=' Yes'
+no='󰜺 Cancel'
 
 
 # Execute Command
 run_cmd() {
-	selected="$(confirm_exit)"
+	yesno_options="$yes\n$no"
+	selected="$(_runrofimenu "$yesno_options" "Are you sure?" "")"
 	if [[ "$selected" == "$yes" ]]; then
 		if [[ $1 == '--shutdown' ]]; then
 			custom-close-hypr-windows
@@ -66,8 +36,9 @@ run_cmd() {
 	fi
 }
 
-# Actions
-chosen="$(run_rofi)"
+options=$(echo -e "$lock\n$suspend\n$reboot\n$shutdown")
+chosen="$(_runrofimenu "$options" "Power Menu" "")"
+
 case ${chosen} in
     $shutdown)
 		run_cmd --shutdown
